87c89d17cc8738b2c9afec28fe3de644cd3a346f
John Doe <johndoe@example.com>
2019-07-14 01:46:59 +0300

    a

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
